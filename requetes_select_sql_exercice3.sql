3f67b330756c494096b051f93ed66c4bda57e108
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-09-03 13:28:50 +0200

    Ajout exercice 3 requêtes SELECt

---
+++ requetes_select_sql_exercice3.sql
@@ -0,0 +1,25 @@
+
+1.
+SELECT lccl_id_ligne, ar_libelle, ar_reference, lccl_quantite_commandee, 
+lccl_prix_vente, (lccl_quantite_commandee * lccl_prix_vente) as montant_total
+FROM gd_ligne_commande_client JOIN gd_article
+ON lccl_id_article = ar_id_article
+WHERE lccl_id_commande = 1
+
+2.
+SELECT cl_nom_client, COUNT(cocl_id_commande)
+FROM gd_commande_client JOIN gd_client
+ON cocl_id_client = cl_id_client
+GROUP BY cl_nom_client 
+
+3.
+SELECT ar_libelle, SUM(lr_quantite_recue) AS quantite_totale_recue
+FROM gd_ligne_reception JOIN gd_article
+ON lr_id_article = ar_id_article
+GROUP BY ar_libelle
+
+4.
+SELECT ar_libelle, st_quantite, ar_seuil_alerte
+FROM gd_stock JOIN gd_article
+ON st_id_article = ar_id_article
+WHERE (st_quantite <= ar_seuil_alerte)
