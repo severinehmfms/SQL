2ce304dd1d1e926ebab8a44dfa4456c90d274e36
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-09-03 15:45:22 +0200

    tentative recursive

--- requetes_select_sql_exercice3.sql
+++ requetes_select_sql_exercice3.sql
@@ -18,7 +18,6 @@ FROM gd_client LEFT JOIN gd_commande_client
 ON cocl_id_client = cl_id_client
 GROUP BY cl_id_client;
 
-
 --3.
 SELECT ar_libelle, SUM(lr_quantite_recue) AS quantite_totale_recue
 FROM gd_ligne_reception JOIN gd_article
@@ -31,6 +30,36 @@ FROM gd_stock JOIN gd_article
 ON st_id_article = ar_id_article
 WHERE (st_quantite <= ar_seuil_alerte)
 
+--5. Requête récursive
+--sans récursif
+SELECT `co_id_contenant_parent` as niveau, `ar_reference`,  `ar_libelle`, `co_type`, `co_code_barres`, `st_quantite`, `em_zone`, `em_allee`, `em_travee`, `em_niveau`
+FROM gd_stock 
+JOIN gd_article ON st_id_article = ar_id_article 
+JOIN gd_contenant ON st_id_contenant = co_id_contenant
+JOIN gd_emplacement ON co_id_emplacement = em_id_emplacement
+WHERE ar_id_article=1;
 
+--avec récursif (ne fonctionne pas encore)
+WITH RECURSIVE container_hierarchy AS (
+   -- Étape de base 
+    SELECT co_id_contenant, co_id_contenant_parent, `ar_reference`,  `ar_libelle`, `co_type`, `co_code_barres`, `st_quantite`, 
+	`em_zone`, `em_allee`, `em_travee`, `em_niveau`
+	FROM gd_stock 
+	JOIN gd_article ON st_id_article = ar_id_article 
+	JOIN gd_contenant ON st_id_contenant = co_id_contenant
+	JOIN gd_emplacement ON co_id_emplacement = em_id_emplacement
+	WHERE ar_id_article=1
+ 
+   UNION ALL
+ 
+   -- Étape récursive : on remonte au contenant parent (ex: du colis vers le bac, puis du bac vers la palette)
+  SELECT  co_id_contenant, co_id_contenant_parent, `ar_reference`,  `ar_libelle`, `co_type`, `co_code_barres`, `st_quantite`, `em_zone`, `em_allee`, `em_travee`, `em_niveau`
+    FROM container_hierarchy
+    JOIN gd_stock ON co_id_contenant = co_id_contenant_parent
+	JOIN gd_article ON st_id_article = ar_id_article 
+	JOIN gd_contenant ON st_id_contenant = co_id_contenant
+	JOIN gd_emplacement ON co_id_emplacement = em_id_emplacement        
+)
 
-
+SELECT *
+FROM container_hierarchy;
